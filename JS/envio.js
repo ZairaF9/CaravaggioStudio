74b395286ccfd769a1efcd687e8a711b0ece0d3d
ZairaF9 <98000015+ZairaF9@users.noreply.github.com>
2023-12-28 13:13:43 -0600

    d

--- JS/envio.js
+++
@@ -1,6 +0,0 @@
-const agregar=document.getElementById("input_num_regalo");
-
-function tarjeta(){
-
-    agregar.innerHTML="<input type='text' class='form-control' id='regalo_input_id' name='regalo_input_name' placeholder='Numero'><button class='btn btn-outline-primary btn-block' id='btn_regalo_id'>Usar</button>";
-}
